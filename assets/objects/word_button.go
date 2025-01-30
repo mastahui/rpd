f9b3344a174d5674df7692dc2404554d5a07b913
mastahui <dshinghui@gmail.com>
2025-01-30 15:37:16 -0500

    main commit

---
+++ assets/objects/word_button.go
@@ -0,0 +1,31 @@
+components {
+  id: "text"
+  component: "/main/scripts/text.script"
+}
+embedded_components {
+  id: "gameWord"
+  type: "label"
+  data: "size {\n"
+  "  x: 128.0\n"
+  "  y: 32.0\n"
+  "}\n"
+  "text: \"Label\"\n"
+  "font: \"/builtins/fonts/default.font\"\n"
+  "material: \"/builtins/fonts/label-df.material\"\n"
+  ""
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"text_label\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/sprites/sprites_atlas.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.25
+    y: 0.25
+  }
+}
